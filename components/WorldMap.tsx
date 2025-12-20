5b272847173654724b311b5390a49a2bc8fea3e6
Taha Rawjani <tahakazim08@gmail.com>
2025-12-20 15:42:08 -0500

    bugs

--- components/WorldMap.tsx
+++ components/WorldMap.tsx
@@ -28,7 +28,7 @@ export default function WorldMap() {
   useEffect(() => {
     // Fix for default marker icon issue in Next.js
     if (typeof window !== "undefined") {
-      delete L.Icon.Default.prototype._getIconUrl;
+      delete (L.Icon.Default.prototype as any)._getIconUrl;
       L.Icon.Default.mergeOptions({
         iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
         iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
